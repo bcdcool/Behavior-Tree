2ad235be9ceab9b8f2d6145bdb17f163f0842424
Brandon <b.c.dunlap@outlook.com>
2016-10-30 20:11:07 -0400

    Node Work

--- Assets/MyBehaviorTree.cs
+++ Assets/MyBehaviorTree.cs
@@ -5,10 +5,12 @@ using TreeSharpPlus;
 
 public class MyBehaviorTree : MonoBehaviour
 {
-	public Transform wander1;
-	public Transform wander2;
-	public Transform wander3;
-	public GameObject participant;
+    public GameObject[] movePoints; 
+
+    public GameObject[] participants;
+
+    public GameObject ball; 
+
 
 	private BehaviorAgent behaviorAgent;
 	// Use this for initialization
@@ -25,19 +27,49 @@ public class MyBehaviorTree : MonoBehaviour
 	
 	}
 
-	protected Node ST_ApproachAndWait(Transform target)
+	protected Node ST_ApproachAndWait(Transform target, GameObject agent)
 	{
 		Val<Vector3> position = Val.V (() => target.position);
-		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+		return new Sequence( agent.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
 	}
 
-	protected Node BuildTreeRoot()
+    protected Node ST_ApproachAndPickupBall()
+    {
+        Val<Vector3> position = Val.V(() => ball.transform.position);
+        return new Sequence(participant1.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+
+    protected Node ST_ThrowAndCatch()
+    {
+        Val<Vector3> position = Val.V(() => ball.transform.position);
+        return new Sequence(participant1.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+
+    }
+
+    protected Node ST_StartleAndRun()
+    {
+        Val<Vector3> position = Val.V(() => ball.transform.position);
+        return new Sequence(participant1.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Node roaming = new DecoratorLoop(
+            this.ST_ApproachAndWait(this.wander1.transform);
+        return roaming; 
+    }
+
+    /*
+    protected Node BuildTreeRoot()
 	{
 		Node roaming = new DecoratorLoop (
 						new SequenceShuffle(
-						this.ST_ApproachAndWait(this.wander1),
-						this.ST_ApproachAndWait(this.wander2),
-						this.ST_ApproachAndWait(this.wander3)));
+						this.ST_ApproachAndWait(this.wander1.transform),
+						this.ST_ApproachAndWait(this.wander2.transform),
+						this.ST_ApproachAndWait(this.wander3.transform)));
 		return roaming;
 	}
+    */
 }
